8a2d3e93272198a8abfe7693bfc78748b0302220
EkaterinaMelnik <melnykkatia@gmail.com>
2018-11-03 21:46:51 +0200

    update Hello componennt

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -11,7 +11,6 @@ function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
     if (enthusiasmLevel <= 0) {
         throw new Error('You could be a little more enthusiastic. :D');
     }
-
     return (
         <div className="hello">
             <div className="greeting">
